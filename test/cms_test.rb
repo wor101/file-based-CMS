b0d1e28302f8223c321363826f070d9721535e46
Will <wor101@gmail.com>
2021-04-21 20:31:12 +0000

    added cms_test with assertions to test index and content pages

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -4,8 +4,29 @@ require 'minitest/autorun'
 require 'rack/test'
 
 
-require_relative 'cms'
+require_relative '../cms'
 
-class CmsTest < MiniTest::Test
+class CMSTest < MiniTest::Test
+  include Rack::Test::Methods
+  
+  def app 
+    Sinatra::Application
+  end
+  
+  def test_index
+    get '/'
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, "about.txt")
+    assert_includes(last_response.body, "changes.txt")
+    assert_includes(last_response.body, "history.txt")
+  end  
+
+  def test_content
+    get '/about.txt'
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, "The lich is, perhaps, the single most powerful form of undead known to exist.")
+  end
   
 end
