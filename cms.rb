52968794fbaf911088412512529eae36cde540b7
Will <wor101@gmail.com>
2021-04-22 21:00:27 +0000

    Implemented Markdown to HTML viewing

--- cms.rb
+++ cms.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'redcarpet'
 
 configure do
   enable :sessions
@@ -9,6 +10,21 @@ configure do
 end
 
 root = File.expand_path("..", __FILE__) # sets root directory for project
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file(file_path)
+  file = File.read(file_path)
+  
+  if file_path[-3..-1] == '.md'
+    render_markdown(file)
+  else
+    file
+  end
+end
 
 get '/' do
   @file_names = Dir.children(root + "/data/")
@@ -19,16 +35,26 @@ end
 
 get '/:file_name' do
   file_name = params[:file_name]
+  file_path = root + "/data/" + file_name
   
+  if File.exist?(file_path)
+    load_file(file_path)
+  else
+    session[:error] = "#{file_name} does not exist."
+    redirect '/'
+  end
+  
+=begin
   if Dir.children(root + "/data/").none?(file_name)
     session[:error] = "#{file_name} does not exist."
     redirect '/'
+  elsif file_name[-3..-1] == '.md'
+    render_markdown(load_file(file_name))
   else
-    file = File.new(root + "/data/#{file_name}")
-    
     # headers["Content-Type"] = "text/plain" # this will tell page to display everything as txt (already using erb template so no point)
-    @file_data = file.read
+    @file_data = load_file(file_name)
+    erb :content, layou: :layout
   end
+=end
 
-  erb :content, layou: :layout
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -20,6 +20,7 @@ class CMSTest < MiniTest::Test
     assert_includes(last_response.body, "about.txt")
     assert_includes(last_response.body, "changes.txt")
     assert_includes(last_response.body, "history.txt")
+    assert_includes(last_response.body, "markdown_test.md")
   end  
 
   def test_content
@@ -36,6 +37,14 @@ class CMSTest < MiniTest::Test
     get last_response["Location"]
     assert_equal(200, last_response.status)
     assert_includes(last_response.body, "dragon.txt does not exist.")
+  end
+  
+  def test_markdown_file
+    get '/markdown_test.md'
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, "<h1>h1 Heading 8-)</h1>")
+    
   end
   
 end
